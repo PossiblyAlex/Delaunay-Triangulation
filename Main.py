48f892e24e02d58c67f486c94cda85a800eb6ff6
Alex <alex.leisure125@gmail.com>
2020-02-26 14:48:57 -0600

    Makes polygon with points you add to list. Doesn't really work the way I want it to

--- Main.py
+++ Main.py
@@ -12,12 +12,17 @@ def leftClick(event):
     print("Left")
     print(event.x)
     print(event.y)
-    points.append((event.x, event.y))
-    x=event.x
-    y=event.y
-    r=4
+    x = event.x
+    y = event.y
+    r = 4
+    points.append(x)
+    points.append(y)
     canvas.create_oval(x-r, y-r, x+r, y+r, fill="red")
     canvas.pack()
+    if (len(points)/2) > 2:
+        test = canvas.create_polygon(*points, fill="blue")
+        canvas.tag_lower(test)
+    """print(points)"""
 
 
 canvas.bind("<Button-1>", leftClick)
