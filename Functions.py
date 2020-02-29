50a4091537a22a50ef189a6907f86656f97e36b7
Alex <alex.leisure125@gmail.com>
2020-02-29 15:33:50 -0600

    Added triangle area function to Functions.py

--- Functions.py
+++ Functions.py
@@ -1,2 +1,13 @@
-def test():
-    print("test")
+
+
+# This function will return 2 times the signed area of a triangle
+# defined by tuples a,b,c in the format of (int,int).
+# If triangle abc defines a counter-clockwise circuit, it returns a positive number
+# If triangle abc defines a clockwise circuit, it returns a negative number
+def area(a, b, c):
+    # IMPORTANT: tkinter has flipped y axis.
+    # When flipped, clockwise ordered points will be counter clockwise and vice versa.
+    # To compensate for this, I will make y values negative, which will factor out to
+    # the negation of the whole area
+    two_area = a[0]*b[1] - a[1]*b[0] + a[1]*c[0] - a[0]*c[1] + b[0]*c[1] - b[1]*c[0]
+    return two_area * -1
