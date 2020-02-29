424e01b2c5edd70a86255553d1e438011d1394f8
Alex <alex.leisure125@gmail.com>
2020-02-29 15:54:38 -0600

    Added changes to Main

---
+++ Main.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+import Functions as f
+
+print("Program is running...")
+root = tk.Tk()
+canvas = tk.Canvas(root, width=1045, height=554, bg="white")
+canvas.pack()
+
+points = []
+adjMatrix = []
+
+
+def add_point(point):
+    points.append(point)
+
+    for col in adjMatrix:
+        col.append(0)
+
+    tempCol = [0] * len(points)
+    adjMatrix.append(tempCol)
+
+    if len(points) > 1:
+        adjMatrix[len(points)-2][len(points)-1] = 1
+
+    print(points)
+    if len(points) == 3:
+        area = f.area(points[0], points[1], points[2])
+        print("2A = " + str(area))
+
+    for x in adjMatrix:
+        print(x)
+
+
+def left_click(event):
+    print("Left")
+    print(event.x)
+    print(event.y)
+    x = event.x
+    y = event.y
+
+    add_point((x,y))
+    print(len(points))
+    if len(points) > 2:
+        print("todo")
+    update()
+    """print(points)"""
+
+
+def enter_press(event):
+    global points
+    global adjMatrix
+    points = []
+    adjMatrix = []
+    update()
+
+
+def update():
+    for item in canvas.find_all():
+        canvas.delete(item)
+
+    r = 4
+    for point in points:
+        canvas.create_oval(point[0]-r, point[1]-r, point[0]+r, point[1]+r, fill="red")
+
+    for i, col in enumerate(adjMatrix):
+        for j, edge in enumerate(col):
+            if edge == 1:
+                canvas.create_line(points[i][0], points[i][1], points[j][0], points[j][1])
+    canvas.pack()
+
+
+canvas.bind("<Button-1>", left_click)
+canvas.bind_all("<Return>", enter_press)
+
+root.mainloop()
+
