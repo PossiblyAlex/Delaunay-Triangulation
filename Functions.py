e326f923c8e938c8b65110968c0d147ac0c19e13
Alex <alex.leisure125@gmail.com>
2020-02-29 16:48:33 -0600

    added between function

--- Functions.py
+++ Functions.py
@@ -25,3 +25,18 @@ def left(a, b, c):
 def colinear(a, b, c):
     is_cl = area(a, b, c) == 0
     return is_cl
+
+
+# returns true iff abc are colinear and point c
+# lies on the closed segment ab
+# so, c[0] must be somewhere between a[0] and b[0]; likewise with the y coordinate
+def between(a, b, c):
+    # if points are not coliniear, trivial case and we're done
+    if not colinear(a, b, c):
+        return False
+
+    # if ab not vertical, check betweenness of x; else on y
+    if a[0] != b[0]:
+        return (a[0] <= c[0] <= b[0]) or (a[0] >= c[0] >= b[0])
+    else:
+        return (a[1] <= c[1] <= b[1]) or (a[1] >= c[1] >= b[1])
